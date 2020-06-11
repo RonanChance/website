55b7749b75e27ef1401bc15ee71da67ab466d0ab
Komodo <pareisago@gmail.com>
2020-06-11 11:59:33 -0400

    A few more necessary changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,14 +4,15 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "7f1df1ee47854388e684afc0845ea820",
+"assets/LICENSE": "ec1b69058122f16bec2aa343a1a9fbcc",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/rd.jpg": "975b955f594982ccd7d48752b435a9f6",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "e583cc061be93e9aa2561c132b6803fb",
 "/": "e583cc061be93e9aa2561c132b6803fb",
-"main.dart.js": "2064086f7beba0e5550a74b0b08e1945",
+"main.dart.js": "5a70703f1d742ef75687c4f4db48251e",
 "manifest.json": "a12712bc2f0379d490e1fb88ac9400bf"
 };
 

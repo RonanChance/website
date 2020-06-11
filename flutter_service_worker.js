18e0f953463e5335a28b591d9638e7d6847a9306
Komodo <pareisago@gmail.com>
2020-06-11 15:12:45 -0400

    Some changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,14 +6,13 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/LICENSE": "ec1b69058122f16bec2aa343a1a9fbcc",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/rd.jpg": "975b955f594982ccd7d48752b435a9f6",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "e583cc061be93e9aa2561c132b6803fb",
-"/": "e583cc061be93e9aa2561c132b6803fb",
+"index.html": "2965f2b20a37c62d22d3053f45c5aaca",
+"/": "2965f2b20a37c62d22d3053f45c5aaca",
 "main.dart.js": "5a70703f1d742ef75687c4f4db48251e",
-"manifest.json": "a12712bc2f0379d490e1fb88ac9400bf"
+"manifest.json": "cc7d4fad20af6267a747d70286ef8af1"
 };
 
 self.addEventListener('activate', function (event) {

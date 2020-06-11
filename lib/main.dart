faef1369e65bd4828ea4d674af62c8f0114fba0c
Komodo <pareisago@gmail.com>
2020-06-11 12:06:36 -0400

    Adjusting file structure

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:website/clipper.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:html' as html;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Ronan Chance',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              height: 750,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF090B61),
+                    Color(0xFF7AAEFF),
+                  ],
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 250,
+                  ),
+                  Positioned(
+                    top: 20,
+                    left: 150,
+                    child: Text(
+                      "Ronan Chance Donovan",
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 100,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      MaterialButton(
+                        onPressed: () {
+                          html.window
+                              .open("https://github.com/RonanChance", "Git");
+                        },
+                        child: Text("Github"),
+                        color: Colors.white,
+                      ),
+                      MaterialButton(
+                        onPressed: () {
+                          html.window
+                              .open("https://github.com/RonanChance", "Git");
+                        },
+                        child: Text("Github"),
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Container(
+        height: 100.0,
+        width: 100.0,
+        child: FittedBox(
+          child:
+              FloatingActionButton(child: Icon(Icons.email), onPressed: () {}),
+        ),
+      ),
+    );
+  }
+}
